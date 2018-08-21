a1f2cd4f3b7e7b200111b5caa954037c6fb5ddd1
paulinetrinh <ptrinh88@uw.edu>
2018-08-21 15:00:12 -0700

    transferring local git q2-breakaway file to statdivlab/q2-breakaway

---
+++ README.md
@@ -0,0 +1,15 @@
+# breakaway QIIME 2 plugin
+
+[![Build Status](https://travis-ci.org/qiime2/q2-breakaway.svg?branch=master)](https://travis-ci.org/qiime2/q2-breakaway)
+[![Coverage Status](https://coveralls.io/repos/github/qiime2/q2-breakaway/badge.svg?branch=master)](https://coveralls.io/github/qiime2/q2-breakaway?branch=master)
+
+This package contains the breakaway QIIME 2 plugin. To learn how to use this, see https://qiime2.org.
+
+Install phyloseq 
+```conda install -c bioconda -c conda-forge bioconductor-phyloseq```
+
+jsonlite
+```conda install -c r r-jsonlite```
+
+Install devtools
+```conda install -c r r-devtools``` 
---
+++ q2_breakaway/plugin_setup.py
@@ -0,0 +1,60 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2018, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+import importlib
+
+import qiime2.plugin
+from qiime2.plugin import Properties, Choices, Str
+
+from q2_types.sample_data import SampleData, AlphaDiversity
+from q2_types.feature_data import FeatureData, Sequence
+from q2_types.feature_table import FeatureTable, Frequency
+
+import q2_breakaway
+
+plugin = qiime2.plugin.Plugin(
+    name='breakaway',
+    version=q2_breakaway.__version__,
+    website='http://github.com/adw96/breakaway/',
+    package='q2_breakaway',
+    description=('baway'),
+    short_description='ba.',
+    citations=qiime2.plugin.Citations.load('citations.bib', package='q2_breakaway')
+)
+
+_METRIC_CHOICES = {'richness', 'chao_bunge'}
+
+plugin.methods.register_function(
+    function=q2_breakaway.alpha,
+    inputs={'table': FeatureTable[Frequency]},
+    outputs=[('alpha_diversity', SampleData[AlphaDiversity % Properties(["StandardError", "LowerConfidence", "UpperConfidence", "SampleNames", "MethodName", "ModelType"])])],
+    parameters={'metric': Str % Choices(_METRIC_CHOICES)},
+    input_descriptions={
+        'table': ('The feature table containing the samples for which alpha '
+                  'diversity should be computed.')
+    },
+    parameter_descriptions={
+        'metric': 'An alpha diversity metric',
+    },
+    output_descriptions={
+        'alpha_diversity': 'Vector containing per-sample alpha diversities.'
+    },
+    name='Richness, better',
+    description='Amy\'s opinionated ideas about richness as a q2, '
+                'plug-in.'
+)
+
+plugin.visualizers.register_function(
+    function=q2_breakaway.plot,
+    inputs={'alpha_diversity': SampleData[AlphaDiversity % Properties(["StandardError", "LowerConfidence", "UpperConfidence", "SampleNames", "MethodName", "ModelType"])]},
+    parameters={},
+    input_descriptions={
+        'alpha_diversity': ('Vector containing per-sample breakaway generated alpha diversities.')
+    },
+    name='Richness plot, better',
+    description='Making better plots'
+)
